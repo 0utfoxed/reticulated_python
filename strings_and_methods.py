f6e77eef663c46cc15367d381e07bf4c4132a261
Gregg Fox <gregg.c.fox@gmail.com>
2020-06-20 18:42:55 -0500

    strings and methods

---
+++ strings_and_methods.py
@@ -0,0 +1,15 @@
+import builtins
+
+string = '''This    is     a    string    with   whitespace.'''
+
+print(string)
+
+string2 = "This is a long string \
+broken across \
+several lines of code."
+
+print(string2)
+
+string3 = 'abc'
+
+print(string3.upper())
