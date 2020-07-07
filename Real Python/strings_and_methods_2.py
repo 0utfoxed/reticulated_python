811accff1ef92618f2fdd1355bf07b8009dcb1c6
Gregg Fox <gregg.c.fox@gmail.com>
2020-07-07 09:54:23 -0500

    A short word on alternative quoting in string declarations.

---
+++ Real Python/strings_and_methods_2.py
@@ -0,0 +1,9 @@
+# Practice: alternative means of printing double quotes within a string
+
+method1 = 'He said, "She went to the market a while ago."'
+ 
+print(method1)
+ 
+method2 = "He said, \"She went to the market a while ago.\""
+ 
+print(method2)
