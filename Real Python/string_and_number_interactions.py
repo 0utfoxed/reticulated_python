994be266719302bdee2a6b28586dfe81a216895c
Gregg Fox <gregg.c.fox@gmail.com>
2020-07-07 11:11:24 -0500

    int() and float()

---
+++ Real Python/string_and_number_interactions.py
@@ -0,0 +1,25 @@
+'''
+
+Review 1:
+Create a string object that stores an integer as its value, then convert that string into an actual integer object using int() ; test that your new object is really a number by multiplying it by another number and displaying the result.
+
+'''
+
+number = "12"
+
+real_number = int(number)
+
+print(real_number*3)
+
+'''
+
+Review 2:
+Create a string object and an integer object, then display them side-by-side with a single print statement by using the str() function
+
+'''
+
+string = "This is the number"
+
+integer = 2
+
+print(string, integer)
