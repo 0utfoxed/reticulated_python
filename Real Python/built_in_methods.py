c8eb56f4c5cdc11cca761a52003300c45094f14a
Gregg Fox <gregg.c.fox@gmail.com>
2020-07-07 10:25:54 -0500

    Practice with len() and string concatenation

---
+++ Real Python/built_in_methods.py
@@ -0,0 +1,39 @@
+# my_string = 'abc'
+
+# string_length = len(my_string)
+
+# print(string_length)
+
+# Experiment 2: adding string lengths separately and corporately
+
+'''
+
+my_string_1 = "abra"
+
+my_string_2 = "kadabra"
+
+magic_string = my_string_1 + my_string_2
+
+length_1 = len(my_string_1)
+
+length_2 = len(my_string_2)
+
+print(len(magic_string))
+
+print(length_1 + length_2)
+
+# Intro to indexing
+
+print(magic_string[1:4])
+
+flavor = "birthday cake"
+
+print(flavor[0:3])
+
+print(flavor[:5])
+
+'''
+
+text = "bazinga"
+
+print(text[2:6])
