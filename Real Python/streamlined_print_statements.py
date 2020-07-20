e6543b9f35842a77c1119b88d41cbfe1f6c488f8
Gregg Fox <gregg.c.fox@gmail.com>
2020-07-20 17:53:09 -0500

    the format() method and streamlined prints

---
+++ Real Python/streamlined_print_statements.py
@@ -0,0 +1,15 @@
+# Streamlined printing using the format() method.
+
+# Method 1
+peaches = 2
+apricots = 3
+
+print("She has {} peaches and {} apricots.".format(peaches, apricots))
+
+# Method 1 Mod using indexing
+
+print("She has {0} peaches and {1} apricots.".format(peaches, apricots))
+
+# Method 2
+
+print(f"She has {peaches} peaches and {apricots} apricots.")
